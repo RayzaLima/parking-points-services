355f0af2c4c2543aa2f8cadab179c383a882c231
RayzaLima <rayzalima2@gmail.com>
2022-05-17 22:31:51 -0300

    Ajuste Controller

--- src/main/java/parkingpointsservices/controller/ParkingController.java
+++ src/main/java/parkingpointsservices/controller/ParkingController.java
@@ -1,4 +1,4 @@
-package parkingpointsservices.controler;
+package parkingpointsservices.controller;
 
 import org.springframework.stereotype.Controller;
 //import org.springframework.web.bind.annotation.PostMapping;
