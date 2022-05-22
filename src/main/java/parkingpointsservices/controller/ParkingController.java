e8eae03f1289f9c7861dd2715b805ccdf551de99
RayzaLima <rayzalima2@gmail.com>
2022-05-21 23:27:24 -0300

    controller implements

--- src/main/java/parkingpointsservices/controller/ParkingController.java
+++ src/main/java/parkingpointsservices/controller/ParkingController.java
@@ -2,16 +2,36 @@ package parkingpointsservices.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
 //import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+import parkingpointsservices.model.Usuario;
 import parkingpointsservices.service.UsuarioLocalService;
 
 @Controller
 @RequestMapping(path = "/parkingpoints")
 public class ParkingController {
 
+    @PostMapping(path = "/usuario")
+    public @ResponseBody Usuario adicionarUsuario(@ResponseBody) {
+        Usuario usuario = new Usuario();
+
+        usuario.setNome(nome);
+        usuario.setCpf(cpf);
+        usuario.setTelefone(telefone);
+        usuario.setEmail(email);
+        usuario.setSenha(senha);
+
+        return usuarioService.criarUsuario(usuario);
+    }
+
     @Autowired
     private UsuarioLocalService usuarioService;
+
+    public ParkingController(UsuarioLocalService usuarioService) {
+        this.usuarioService = usuarioService;
+    }
 
 }
