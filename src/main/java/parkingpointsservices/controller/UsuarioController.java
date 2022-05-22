c2522a02729330f7d24c3fa77b6f383e4961cda1
RayzaLima <rayzalima2@gmail.com>
2022-05-22 17:09:03 -0300

    Set controller name to UserController

--- src/main/java/parkingpointsservices/controller/UsuarioController.java
+++ src/main/java/parkingpointsservices/controller/UsuarioController.java
@@ -2,18 +2,21 @@ package parkingpointsservices.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import parkingpointsservices.dto.UsuarioDTO;
+import parkingpointsservices.exception.UsuarioException;
 import parkingpointsservices.model.Usuario;
 import parkingpointsservices.service.UsuarioLocalService;
 
 @Controller
 @RequestMapping(path = "/parkingpoints")
-public class ParkingController {
+public class UsuarioController {
 
     @PostMapping(path = "/usuario")
     public @ResponseBody Usuario adicionarUsuario(@RequestBody UsuarioDTO usuarioDTO) {
@@ -26,6 +29,16 @@ public class ParkingController {
         usuario.setSenha(usuarioDTO.getSenha());
 
         return usuarioLocalService.criarUsuario(usuario);
+    }
+
+    @DeleteMapping(path = "/usuario/{id}")
+    public @ResponseBody String deletarUsuario(@PathVariable("id") String id) {
+        try {
+            usuarioLocalService.deletarUsuario(Integer.parseInt(id));
+            return "Usuário Deletado com Sucesso";
+        } catch (UsuarioException e) {
+            return e.getMessage();
+        }
     }
 
     @Autowired
